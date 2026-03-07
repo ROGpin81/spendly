93da3ab3d655bc22ba75c0742b36130650fd04a8
ROGpin81 <roger.pinedab@gmail.com>
2026-03-07 11:11:50 -0600

    Add DB definition of categorias table

--- backend/models/Categorias.js
+++ backend/models/Categorias.js
@@ -1,12 +1,38 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db/conexion');
 
-export default class Categorias extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
+const Categoria = sequelize.define('categoria', {
+
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+
+  name: {
+    type: DataTypes.STRING(60),
+    allowNull: false
+  },
+
+  owner_user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW
+  },
+
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: true
   }
-}
+
+}, {
+  tableName: 'categorias',
+  timestamps: false
+});
+
+module.exports = Categoria;
